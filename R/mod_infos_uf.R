edcf471a217a187184e5cd5e62636793bea1eda0
Maykon G. Pedro <maykonglaffite@gmail.com>
2021-11-21 21:11:32 -0300

    add gráfico de colunas highcharts

--- R/mod_infos_uf.R
+++ R/mod_infos_uf.R
@@ -39,10 +39,10 @@ mod_infos_uf_ui <- function(id, base){
             column(width = 6),
             box(
                 width = 6,
-                # plotOutput(outputId = ns("mapa_relatorios"), height = "800px")
+                plotOutput(outputId = ns("plot_genero"))
                 # plotOutput(outputId = ns("mapa_relatorios"))
-                highcharter::highchartOutput(outputId = ns("mapa_relatorios"), 
-                                             height = "800px")
+                # highcharter::highchartOutput(outputId = ns("mapa_relatorios"), 
+                #                              height = "800px")
             )
         )
     )
@@ -53,7 +53,82 @@ mod_infos_uf_ui <- function(id, base){
 
 mod_infos_uf_server <- function(id, bases) {
     moduleServer(id, function(input, output, session) {
-        output$grafico <- highcharter::renderHighchart({
+        
+        # output$plot_genero <- renderPlot({
+        #     
+        #     tb_plot <- bases |> 
+        #         purrr::pluck("dados_uf") |> 
+        #         dplyr::filter(
+        #             ano_base == "2019",
+        #             mapeamento == "IBÁ - Relatório Anual 2020",
+        #             # uf %in% c("PR", "SC", "RS")
+        #             uf %in% unique(bases$dados_uf$uf)
+        #             ) |> 
+        #         # dplyr::filter(
+        #         #     ano_base == input$ano_base,
+        #         #     mapeamento == input$nome_mapeamento,
+        #         #     uf %in% input$uf
+        #         #     ) |>
+        #         dplyr::group_by(uf, genero) |> 
+        #         dplyr::summarise(area_tot = sum(area_ha, na.rm = TRUE))
+        #         
+        #     tb_plot |> 
+        #         ggplot2::ggplot(
+        #             ggplot2::aes(
+        #                 x = uf,
+        #                 y = area_tot,
+        #                 fill = genero
+        #             )
+        #         ) +
+        #         ggplot2::geom_col() +
+        #         ggplot2::scale_fill_viridis_d()
+        #         
+        #     
+        # })
+        
+        output$plot_genero <- highcharter::renderHighchart({
+            
+            tb_plot <- bases |> 
+                purrr::pluck("dados_uf") |> 
+                dplyr::filter(
+                    ano_base == "2019",
+                    mapeamento == "IBÁ - Relatório Anual 2020",
+                    # uf %in% c("PR", "SC", "RS")
+                    uf %in% unique(bases$dados_uf$uf)
+                ) |> 
+                # dplyr::filter(
+                #     ano_base == input$ano_base,
+                #     mapeamento == input$nome_mapeamento,
+                #     uf %in% input$uf
+                #     ) |>
+                dplyr::group_by(uf, genero) |> 
+                dplyr::summarise(area_tot = sum(area_ha, na.rm = TRUE)) |> 
+                dplyr::arrange(dplyr::desc(area_tot))
+            
+            tb_plot |>
+                highcharter::hchart(
+                    type = "column",
+                    highcharter::hcaes(
+                        x = uf,
+                        y = area_tot,
+                        group = genero
+                    ),
+                    stacking = "normal"
+                    ) |> 
+                # highcharter::hc_colors(
+                #     #definir cores
+                #     colors = c('#000004', '#B63679', '#FCFDBF')
+                # ) |>
+                # highcharter::hc_xAxis(title = "Estado") |> 
+                # highcharter::hc_yAxis(title = "Área em hectares") |> 
+                highcharter::hc_add_theme(highcharter::hc_theme_elementary()) 
+
+  
+            
+
+            
+
+
             
         })
         
