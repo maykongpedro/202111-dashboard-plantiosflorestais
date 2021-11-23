076b10825f16d3a3e25411670f0d0863184d3ac2
Maykon G. Pedro <maykonglaffite@gmail.com>
2021-11-22 23:31:23 -0300

    trocar para o mapa do hoghcharts

--- R/mod_contexto.R
+++ R/mod_contexto.R
@@ -33,10 +33,10 @@ mod_contexto_ui <- function(id, dados) {
         fluidRow(
             column(width = 6),
             box(width = 6,
-                plotOutput(outputId = ns("mapa_relatorios"), height = "800px")
-                # highcharter::highchartOutput(
-                #     outputId = ns("mapa_relatorios"),
-                #     height = "800px")
+                # plotOutput(outputId = ns("mapa_relatorios"), height = "800px")
+                highcharter::highchartOutput(
+                    outputId = ns("mapa_relatorios"),
+                    height = "800px")
                 )
         )
     )
@@ -47,29 +47,67 @@ mod_contexto_ui <- function(id, dados) {
 mod_contexto_server <- function(id, dados, shape) {
     moduleServer(id, function(input, output, session){
 
-        output$mapa_relatorios <- renderPlot({
-            # dados_uf |> dplyr::glimpse()
-            # base |>
-            # dados_uf |>
-                # dplyr::group_by(mapeamento, genero) |>
-                # dplyr::summarise(area = sum(area_ha, na.rm = TRUE)) |>
-                # #dplyr::filter(mapeamento == "IBÁ - Relatório Anual 2020") |>
-                # # dplyr::filter(mapeamento == "IBÁ - Não identificado")
-                #
-                # dplyr::filter(mapeamento == input$nome_mapeamento)  |>
-                #
-                # ggplot2::ggplot(
-                #     ggplot2::aes(
-                #         x = genero,
-                #         y = area/10^6
-                #     )
-                # ) +
-                # ggplot2::geom_col()
+        # output$mapa_relatorios <- renderPlot({
+        #     # dados_uf |> dplyr::glimpse()
+        #     # base |>
+        #     # dados_uf |>
+        #         # dplyr::group_by(mapeamento, genero) |>
+        #         # dplyr::summarise(area = sum(area_ha, na.rm = TRUE)) |>
+        #         # #dplyr::filter(mapeamento == "IBÁ - Relatório Anual 2020") |>
+        #         # # dplyr::filter(mapeamento == "IBÁ - Não identificado")
+        #         #
+        #         # dplyr::filter(mapeamento == input$nome_mapeamento)  |>
+        #         #
+        #         # ggplot2::ggplot(
+        #         #     ggplot2::aes(
+        #         #         x = genero,
+        #         #         y = area/10^6
+        #         #     )
+        #         # ) +
+        #         # ggplot2::geom_col()
+        # 
+        # 
+        #     # gerar resumo de relatório existente por estado
+        #     tb_relatorio_uf <- dados |>
+        #         dplyr::distinct(mapeamento, uf) |>
+        #         dplyr::filter(!is.na(uf)) |>
+        #         dplyr::group_by(mapeamento) |>
+        #         dplyr::mutate(valor = 1)
+        # 
+        #     # filtrar relatório
+        #     tb_relatorio_selecionado <- tb_relatorio_uf |>
+        #         # dplyr::filter(mapeamento == "IBÁ - Relatório Anual 2020")
+        #         dplyr::filter(mapeamento == input$nome_mapeamento)
+        # 
+        #     # fazer join
+        #     shp_plot <- shape |>
+        #         # shp_br |> 
+        #         dplyr::left_join(tb_relatorio_selecionado, by = "uf")
+        # 
+        #     # plotar
+        #     p_map <- shp_plot |>
+        #         sf::st_simplify(dTolerance = 1000) |>
+        #         ggplot2::ggplot()+
+        #         ggplot2::geom_sf(alpha = .5,
+        #                          color = "white",
+        #                          size = 0.2,) +
+        #         ggplot2::geom_sf(
+        #             ggplot2::aes(fill = valor),
+        #             show.legend = FALSE
+        #         ) +
+        #         ggplot2::scale_fill_viridis_c() +
+        #         ggplot2::theme_minimal() +
+        #         ggplot2::theme(axis.text = ggplot2::element_blank())
+        #     
+        #     p_map
+        # 
+        # })
 
+        output$mapa_relatorios <- highcharter::renderHighchart({
 
             # gerar resumo de relatório existente por estado
             tb_relatorio_uf <- dados |>
-                #dados_uf |>
+                # dplyr::filter()
                 dplyr::distinct(mapeamento, uf) |>
                 dplyr::filter(!is.na(uf)) |>
                 dplyr::group_by(mapeamento) |>
@@ -80,67 +118,28 @@ mod_contexto_server <- function(id, dados, shape) {
                 # dplyr::filter(mapeamento == "IBÁ - Relatório Anual 2020")
                 dplyr::filter(mapeamento == input$nome_mapeamento)
 
-            # fazer join
-            shp_plot <- shape |>
-                # shp_br |> 
-                dplyr::left_join(tb_relatorio_selecionado, by = "uf")
+            p_map <- highcharter::hcmap(
+                map = "countries/br/br-all",
+                nullColor = "#d3d3d3",
+                data = tb_relatorio_selecionado,
+                value = "valor",
+                joinBy = c("hc-a2", "uf"),
+                borderColor = "#FAFAFA",
+                borderWidth = 0.1,
+                name = "Estado",
+                dataLabels = list(enabled = TRUE, format = "{point.code}"),
+                tooltip = list(valueDecimals = 0),
+                download_map_data = F
+            ) |>
+                highcharter::hc_legend(ggplot2::element_blank()) |>
+                highcharter::hc_colorAxis(
+                    minColor = "#008d4c",
+                    maxColor = "#008d4c"
+                    )
 
-            # plotar
-            p_map <- shp_plot |>
-                sf::st_simplify(dTolerance = 1000) |>
-                ggplot2::ggplot()+
-                ggplot2::geom_sf(alpha = .5,
-                                 color = "white",
-                                 size = 0.2,) +
-                ggplot2::geom_sf(
-                    ggplot2::aes(fill = valor),
-                    show.legend = FALSE
-                ) +
-                ggplot2::scale_fill_viridis_c() +
-                ggplot2::theme_minimal() +
-                ggplot2::theme(axis.text = ggplot2::element_blank())
-            
             p_map
 
         })
-
-        # output$mapa_relatorios <- highcharter::renderHighchart({
-        #     
-        #     # gerar resumo de relatório existente por estado
-        #     tb_relatorio_uf <- dados |>
-        #         # dplyr::filter()
-        #         dplyr::distinct(mapeamento, uf) |>
-        #         dplyr::filter(!is.na(uf)) |>
-        #         dplyr::group_by(mapeamento) |>
-        #         dplyr::mutate(valor = 1)
-        # 
-        #     # filtrar relatório
-        #     tb_relatorio_selecionado <- tb_relatorio_uf |>
-        #         # dplyr::filter(mapeamento == "IBÁ - Relatório Anual 2020")
-        #         dplyr::filter(mapeamento == input$nome_mapeamento)
-        #     
-        #     p_map <- highcharter::hcmap(
-        #         map = "countries/br/br-all",
-        #         nullColor = "#d3d3d3",
-        #         data = tb_relatorio_selecionado,
-        #         value = "valor",
-        #         joinBy = c("hc-a2", "uf"),
-        #         borderColor = "#FAFAFA",
-        #         borderWidth = 0.1,
-        #         name = "Estado",
-        #         dataLabels = list(enabled = TRUE, format = "{point.code}"),
-        #         tooltip = list(valueDecimals = 0),
-        #         download_map_data = F
-        #     ) |>
-        #         highcharter::hc_legend(ggplot2::element_blank()) |>
-        #         highcharter::hc_colorAxis(
-        #             minColor = "#008d4c",
-        #             maxColor = "#008d4c"
-        #             )
-        #     
-        #     p_map
-        #     
-        # })
 
     })
 }
